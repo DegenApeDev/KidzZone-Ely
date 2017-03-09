d265536c1de2054bf956ea105a01f6db1889e945
linuzo <linuzo@kidzzone.ca>
2017-03-10 07:32:38 +0800

    update for 1.0.4.11

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -75,8 +75,8 @@ namespace pocketmine {
 	const VERSION = "1.0";
 	const API_VERSION = "3.0.0-ALPHA1";
 	const CODENAME = "Birpen";
-	const MINECRAFT_VERSION = "v1.0.0 alpha";
-	const MINECRAFT_VERSION_NETWORK = "1.0.0";
+	const MINECRAFT_VERSION = "v1.0.4 alpha";
+	const MINECRAFT_VERSION_NETWORK = "1.0.4";
 	const GENISYS_API_VERSION = '1.9.3';
 
 	/*
